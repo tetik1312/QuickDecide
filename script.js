9400d657079b2cd49d9276ab45d28fe3301ce8ca
Asya <tetik99@gmx.de>
2023-05-28 16:01:14 +0200

    first questions with sounds

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var sounds = ["assets/kick.mp3"];
 makeClickNoise(document.getElementById("help-icon"));
 makeClickNoise(document.getElementById("ersterButton"));
 makeClickNoise(document.getElementById("close-q"));
+makeClickNoise(document.getElementById("yes"));
+makeClickNoise(document.getElementById("no"));
 function playSample(sounds) {
     var sound = new Audio(sounds);
     sound.play();
+}
+//funktion für klick geräusch
+function makeClickNoise(button) {
+    button.addEventListener("mousedown", function () { playSample("assets/Button_Click.wav"); });
 }
 document.getElementById("help-icon").addEventListener("click", createQuestionbox);
 var questionBox;
@@ -15,7 +21,7 @@ document.getElementById("close-q").addEventListener("click", closeQuestionBox);
 function createQuestionbox() {
     questionBox.classList.add('active-qbox');
     console.log("frage wurde geklickt");
-    box.innerHTML = "hallo";
+    box.innerHTML = "Entscheide dich einfach.";
     questionBox.appendChild(box);
 }
 function closeQuestionBox() {
@@ -24,47 +30,111 @@ function closeQuestionBox() {
         box.innerHTML = "";
     }
 }
+//klick auf start also erstes "JA"
 document.getElementById("ersterButton").addEventListener("click", startGame);
+var gameBox;
+gameBox = document.querySelector("#gamefield");
+var yesnobox = document.createElement("div");
+yesnobox.classList.add("active-game");
 function startGame() {
     console.log("spiel wird gestartet");
+    gameBox.classList.add('active-game');
+    console.log("ja wurde geklickt");
+    yesnobox.innerHTML = questions[qCounter].q;
+    gameBox.appendChild(yesnobox);
+    // timer = setInterval(() => {
+    //     spreadQuestion();
+    //     if(qCounter === questions.length-2) {
+    //         clearInterval(timer);
+    //     }
+    //     qCounter++;
+    //     yesnobox.innerHTML = questions[qCounter].q;
+    // },timerDuration);
 }
-function makeClickNoise(button) {
-    button.addEventListener("mousedown", function () { playSample("assets/kick.mp3"); });
+var qCounter = 0;
+var questions = [
+    { "q": "Wirst du auf die Party gehen?", "sound": "assets/Party.mp3" },
+    { "q": "Möchtest du mit einem neuen Hobby anfangen?", "sound": "assets/Hobby.mp3" },
+    { "q": "Würdest du dich für deinen Traumjob bewerben?", "sound": "assets/Traumjob.mp3" },
+    { "q": "Hast du genug Geld?", "sound": "assets/Geld.mp3" },
+    { "q": "Willst du Heiraten?", "sound": "assets/Heiraten.mp3" },
+    { "q": "Willst du Kinder haben?", "sound": "assets/Kinder.mp3" },
+    { "q": "Bist du glücklich?", "sound": "assets/Glücklich.mp3" },
+    { "q": "Führst du ein erfolgreiches leben?", "sound": "assets/Erfolgreich.mp3" },
+    { "q": "Bereust du etwas in deinem leben?", "sound": "assets/Reue.mp3" },
+    { "q": "Hast du den Sinn des Lebens gefunden?", "sound": "assets/Sinn.mp3" },
+    { "q": "Was ist die Antwort auf das Leben, das Universum und alles?", "sound": "assets/Button_Click.wav" },
+    { "q": "Wer hat die Relativitätstheorie entwickelt?", "sound": "assets/Button_Click.wav" }
+];
+var ybtn = document.getElementById("yes");
+var nbtn = document.getElementById("no");
+var timer;
+var timerDuration = 1000;
+ybtn.addEventListener("click", function () {
+    if (qCounter < questions.length - 1) {
+        deleteQuestion();
+        qCounter++;
+        yesnobox.innerHTML = questions[qCounter].q;
+        clearInterval(timer);
+        if (qCounter > 1) {
+            timer = setInterval(function () {
+                spreadQuestion();
+                if (qCounter === questions.length - 2) {
+                    clearInterval(timer);
+                }
+                qCounter++;
+                yesnobox.innerHTML = questions[qCounter].q;
+            }, timerDuration);
+        }
+    }
+    if (qCounter === questions.length - 1) {
+        clearInterval(timer);
+        yesnobox.innerHTML = "Dein leben liegt in deiner Hand. Wie wirst DU dich entscheiden?";
+        playSample("assets/FinalQuestion.mp3");
+        deleteQuestion();
+    }
+});
+nbtn.addEventListener("click", function () {
+    if (qCounter < questions.length - 1) {
+        deleteQuestion();
+        qCounter++;
+        yesnobox.innerHTML = questions[qCounter].q;
+        clearInterval(timer);
+        if (qCounter > 1) {
+            timer = setInterval(function () {
+                spreadQuestion();
+                if (qCounter === questions.length - 2) {
+                    clearInterval(timer);
+                }
+                qCounter++;
+                yesnobox.innerHTML = questions[qCounter].q;
+            }, timerDuration);
+        }
+    }
+    if (qCounter === questions.length - 1) {
+        clearInterval(timer);
+        yesnobox.innerHTML = "Dein leben liegt in deiner Hand. Wie wirst DU dich entscheiden?";
+        playSample("assets/FinalQuestion.mp3");
+        deleteQuestion();
+    }
+});
+function spreadQuestion() {
+    var unanswered = document.createElement("div");
+    unanswered.classList.add("unanswered");
+    unanswered.innerHTML = questions[qCounter].q;
+    var height = document.body.offsetHeight;
+    var width = document.body.offsetWidth;
+    var hRand = Math.floor(Math.random() * height);
+    var wRand = Math.floor(Math.random() * width);
+    unanswered.setAttribute("style", "top: ".concat(hRand * .8, "px; left: ").concat(wRand * .8, "px;"));
+    document.body.appendChild(unanswered);
+    playSample(questions[qCounter].sound);
 }
-// let clicked: boolean = false;
-// function boxErstellen(): void {
-//     if (clicked == true ) {
-//         let box: HTMLDivElement = document.createElement("div");
-//         box.innerHTML = "";
-//         box.classList.add("feld");
-//         questionBox.appendChild(box);
-//     }
-// }
-// spielButton.addEventListener("click", () => {
-//     // Überprüfen, ob die Spiel-Erklärung bereits vorhanden ist
-//     if (!spielErklaerung.innerHTML) {
-//       // Spiel-Erklärung erstellen
-//       const erklaerungSpan = document.createElement("span");
-//       erklaerungSpan.innerHTML = "Hier steht die Erklärung des Spiels.";
-// function createYesNoButton(callback: (isYes: boolean) => void): void {
-//     const yesButton = document.createElement("button");
-//     yesButton.innerText = "Ja";
-//     yesButton.addEventListener("click", () => {
-//       callback(true);
-//     });
-//     const noButton = document.createElement("button");
-//     noButton.innerText = "Nein";
-//     noButton.addEventListener("click", () => {
-//       callback(false);
-//     });
-//     document.body.appendChild(yesButton);
-//     document.body.appendChild(noButton);
-//   }
-//   createYesNoButton((isYes: boolean) => {
-//     if (isYes) {
-//       console.log("Benutzer hat 'Ja' geklickt");
-//     } else {
-//       console.log("Benutzer hat 'Nein' geklickt");
-//     }
-//   });
+function deleteQuestion() {
+    var unansweredQs = document.getElementsByClassName('unanswered');
+    var allQs = unansweredQs.length;
+    for (var i = 0; i < allQs; i++) {
+        unansweredQs[allQs - i - 1].remove();
+    }
+}
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -2,12 +2,18 @@ var sounds: string[] = ["assets/kick.mp3"];
 makeClickNoise(document.getElementById("help-icon"));
 makeClickNoise(document.getElementById("ersterButton"));
 makeClickNoise(document.getElementById("close-q"));
+makeClickNoise(document.getElementById("yes"));
+makeClickNoise(document.getElementById("no"));
 
 function playSample(sounds: string): void {
     var sound: HTMLAudioElement = new Audio(sounds);
     sound.play();
 }
 
+//funktion für klick geräusch
+function makeClickNoise(button): void {
+    button.addEventListener("mousedown", () => { playSample("assets/Button_Click.wav"); });
+}
 
 document.getElementById("help-icon").addEventListener("click", createQuestionbox)
 var questionBox: HTMLElement;
@@ -20,7 +26,7 @@ document.getElementById("close-q").addEventListener("click", closeQuestionBox);
 function createQuestionbox(): void {
     questionBox.classList.add('active-qbox');
     console.log("frage wurde geklickt");
-    box.innerHTML = "hallo";
+    box.innerHTML = "Entscheide dich einfach.";
     questionBox.appendChild(box);
 }
 
@@ -31,60 +37,120 @@ function closeQuestionBox(): void {
     }
 }
 
+//klick auf start also erstes "JA"
 document.getElementById("ersterButton").addEventListener("click", startGame)
+
+var gameBox: HTMLElement;
+gameBox = document.querySelector("#gamefield");
+let yesnobox: HTMLDivElement = document.createElement("div");
+yesnobox.classList.add("active-game");
 
 function startGame(): void {
     console.log("spiel wird gestartet");
+    gameBox.classList.add('active-game');
+    console.log("ja wurde geklickt");
+    yesnobox.innerHTML = questions[qCounter].q;
+    gameBox.appendChild(yesnobox);
+    // timer = setInterval(() => {
+    //     spreadQuestion();
+    //     if(qCounter === questions.length-2) {
+    //         clearInterval(timer);
+    //     }
+    //     qCounter++;
+    //     yesnobox.innerHTML = questions[qCounter].q;
+    // },timerDuration);
+    
 }
 
-function makeClickNoise(button): void {
-    button.addEventListener("mousedown", () => { playSample("assets/kick.mp3"); });
+let qCounter: number = 0;
+
+const questions: {q: string, sound:string}[] = [
+    {"q": "Wirst du auf die Party gehen?", "sound": "assets/Party.mp3"},
+    {"q": "Möchtest du mit einem neuen Hobby anfangen?", "sound": "assets/Hobby.mp3"},
+    {"q":"Würdest du dich für deinen Traumjob bewerben?", "sound": "assets/Traumjob.mp3"},
+    {"q": "Hast du genug Geld?", "sound": "assets/Geld.mp3"},
+    {"q": "Willst du Heiraten?", "sound": "assets/Heiraten.mp3"},
+    {"q":"Willst du Kinder haben?", "sound": "assets/Kinder.mp3"},
+    {"q": "Bist du glücklich?", "sound": "assets/Glücklich.mp3"},
+    {"q": "Führst du ein erfolgreiches leben?", "sound": "assets/Erfolgreich.mp3"},
+    {"q":"Bereust du etwas in deinem leben?", "sound": "assets/Reue.mp3"},
+    {"q": "Hast du den Sinn des Lebens gefunden?", "sound": "assets/Sinn.mp3"},
+    {"q": "Was ist die Antwort auf das Leben, das Universum und alles?", "sound": "assets/Button_Click.wav"},
+    {"q":"Wer hat die Relativitätstheorie entwickelt?", "sound": "assets/Button_Click.wav"}
+];
+
+const ybtn: HTMLElement = document.getElementById("yes");
+const nbtn: HTMLElement = document.getElementById("no");
+let timer: number;
+let timerDuration:number = 1000;
+
+
+ybtn.addEventListener("click", () => {
+    if (qCounter < questions.length - 1) {
+        deleteQuestion();
+        qCounter++;
+        yesnobox.innerHTML = questions[qCounter].q;
+        clearInterval(timer);
+        if (qCounter > 1){
+            timer = setInterval(() => {
+            spreadQuestion();
+            if(qCounter === questions.length-2) {
+                clearInterval(timer);
+            }
+            qCounter++;
+            yesnobox.innerHTML = questions[qCounter].q;
+        },timerDuration);}
+    }
+    if (qCounter === questions.length - 1) {
+        clearInterval(timer);
+        yesnobox.innerHTML = "Dein leben liegt in deiner Hand. Wie wirst DU dich entscheiden?";
+        playSample("assets/FinalQuestion.mp3");
+        deleteQuestion();
+    }
+});
+
+nbtn.addEventListener("click", () => {
+    if (qCounter < questions.length - 1) {
+        deleteQuestion();
+        qCounter++;
+        yesnobox.innerHTML = questions[qCounter].q;
+        clearInterval(timer);
+        if (qCounter > 1) {
+        timer = setInterval(() => {
+            spreadQuestion();
+            if(qCounter === questions.length-2) {
+                clearInterval(timer);
+            }
+            qCounter++;
+            yesnobox.innerHTML = questions[qCounter].q;
+        },timerDuration);}
+    }
+    if (qCounter === questions.length - 1) {
+        clearInterval(timer);
+            yesnobox.innerHTML = "Dein leben liegt in deiner Hand. Wie wirst DU dich entscheiden?";
+            playSample("assets/FinalQuestion.mp3");
+            deleteQuestion();
+    }
+});
+
+
+function spreadQuestion(): void {
+    let unanswered: HTMLDivElement = document.createElement("div");
+    unanswered.classList.add("unanswered");
+    unanswered.innerHTML = questions[qCounter].q;
+    let height :number = document.body.offsetHeight;
+    let width:number = document.body.offsetWidth;
+    let hRand:number= Math.floor(Math.random()*height);
+    let wRand:number = Math.floor(Math.random()*width);
+    unanswered.setAttribute("style", `top: ${hRand*.8}px; left: ${wRand*.8}px;`);
+    document.body.appendChild(unanswered);
+    playSample(questions[qCounter].sound);
 }
 
-// let clicked: boolean = false;
-
-// function boxErstellen(): void {
-
-//     if (clicked == true ) {
-
-//         let box: HTMLDivElement = document.createElement("div");
-//         box.innerHTML = "";
-//         box.classList.add("feld");
-//         questionBox.appendChild(box);
-//     }
-// }
-
-
-// spielButton.addEventListener("click", () => {
-//     // Überprüfen, ob die Spiel-Erklärung bereits vorhanden ist
-//     if (!spielErklaerung.innerHTML) {
-//       // Spiel-Erklärung erstellen
-//       const erklaerungSpan = document.createElement("span");
-//       erklaerungSpan.innerHTML = "Hier steht die Erklärung des Spiels.";
-
-
-
-// function createYesNoButton(callback: (isYes: boolean) => void): void {
-//     const yesButton = document.createElement("button");
-//     yesButton.innerText = "Ja";
-//     yesButton.addEventListener("click", () => {
-//       callback(true);
-//     });
-    
-//     const noButton = document.createElement("button");
-//     noButton.innerText = "Nein";
-//     noButton.addEventListener("click", () => {
-//       callback(false);
-//     });
-    
-//     document.body.appendChild(yesButton);
-//     document.body.appendChild(noButton);
-//   }
-
-//   createYesNoButton((isYes: boolean) => {
-//     if (isYes) {
-//       console.log("Benutzer hat 'Ja' geklickt");
-//     } else {
-//       console.log("Benutzer hat 'Nein' geklickt");
-//     }
-//   });
+function deleteQuestion(): void {
+    let unansweredQs:HTMLCollection = document.getElementsByClassName('unanswered');
+    let allQs:number = unansweredQs.length;
+    for(let i:number = 0; i < allQs; i++) {
+        unansweredQs[allQs-i-1].remove();
+    }
+}
